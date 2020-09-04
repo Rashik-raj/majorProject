f476dd9baffd507149bfa487eb01cc442adfbe1b
Rashik-raj <rashik123.rs@gmail.com>
2020-09-04 22:38:10 +0545

    ap for online data collection created

--- recognizeMe/urls.py
+++ recognizeMe/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('datacollection/', include('datacollection.urls')),
     path('demo/', include('demo.urls')),
     path('result/',include('result.urls')),
+
 ]
