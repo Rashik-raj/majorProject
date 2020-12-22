2b98cc760fba5fa848e376ce364e9466b7a19ecf
Rashik-raj <rashik123.rs@gmail.com>
2020-12-22 15:48:36 +0545

    demo now shows processed images

--- demo/views.py
+++ demo/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
 from .image_processing import image_preprocessing
 
 # Create your views here.
 def index(request):
-    return render(request,'demo.htm')
+    return render(request, 'demo.htm')
 
 def demo_in_action(request):
-    image_preprocessing()
+    images = image_preprocessing()
     messages.success(request, 'Image Processing has been success!')
-    return redirect('demo')
+    context = {'images':images}
+    return render(request, 'demo.htm', context=context)
