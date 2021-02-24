aa0d4b08f03977642b0a19bd38dff26b0960f293
Rashik-raj <rashik123.rs@gmail.com>
2021-02-24 14:18:43 +0545

    now demo section has upload image functionality, listing unprocessed and processed image

--- demo/urls.py
+++ demo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='demo'),
+    path('upload-demo-image/', views.upload_demo_image, name='upload-demo-image'),
     path('demo-in-action/', views.demo_in_action, name='demo-in-action'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,13 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
 from .image_processing import image_preprocessing
+import os
+import shortuuid
+
+def get_images(folder_name):
+    working_directory = os.getcwd()
+    os.chdir(os.path.join(working_directory, 'online_data_collection', folder_name))
+    images = []
+    for each_img in os.listdir():
+        images.append(each_img)
+    os.chdir(working_directory)
+    return images
 
 # Create your views here.
 def index(request):
-    return render(request, 'demo.htm')
+    unprocessed_images = get_images('image_folder')
+    processed_images = get_images('image_dataset')
+    context = {
+        'unprocessed_images': unprocessed_images,
+        'processed_images': processed_images
+        }
+    return render(request, 'demo.htm', context=context)
 
 def demo_in_action(request):
-    images = image_preprocessing()
-    messages.success(request, 'Image Processing has been success!')
-    context = {'images':images}
+    # images, is_processed = image_preprocessing()
+    is_processed = image_preprocessing()
+    unprocessed_images = get_images('image_folder')
+    processed_images = get_images('image_dataset')
+    if is_processed:
+        messages.success(request, 'Image Processing has been success!')
+    else:
+        messages.warning(request, 'No image to process!')
+    context = {
+        'unprocessed_images': unprocessed_images,
+        'processed_images': processed_images
+        }
     return render(request, 'demo.htm', context=context)
+
+def upload_demo_image(request):
+    if request.method == 'POST':
+        if request.FILES:
+            myfile = request.FILES['data_image']
+            file_extension = myfile.name.split('.')[-1]
+            if file_extension not in ['jpg', 'jpeg', 'png']:
+                messages.error(request, 'Please upload image with ".jpeg", ".jpg" or ".png" extension.')
+            else:
+                file_name = shortuuid.ShortUUID().random(length=8) + '.' + file_extension
+                fs = FileSystemStorage()
+                fs.save('image_folder/'+file_name, myfile)
+                messages.success(request, 'Image Uploaded.')
+        else:
+            messages.error(request, 'Please select an image to upload.')
+    return redirect('/demo')
